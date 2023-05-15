6b8f5354a745bbcf1a92a7d5d9d5c52b0b86d44f
Hamza Khan <hamza-k18@hotmail.com>
2023-05-15 22:54:44 +0000

    completed

--- src/main/java/Application/Service/FlightService.java
+++ src/main/java/Application/Service/FlightService.java
@@ -53,7 +53,7 @@ public class FlightService {
      *         inform our provide the front-end client with information about the added Flight.
      */
     public Flight addFlight(Flight flight){
-        return null;
+        return flightDAO.insertFlight(flight);
     }
 
     /**
@@ -69,7 +69,15 @@ public class FlightService {
      *         unsuccessful. We do this to inform our application about successful/unsuccessful operations. (eg, the
      *         user should have some insight if they attempted to edit a nonexistent flight.)
      */
-    public Flight updateFlight(int flight_id, Flight flight){
+    public Flight updateFlight(int flight_id, Flight flight)
+    {
+        if(flightDAO.getFlightById(flight_id) != null)
+        {
+            flightDAO.updateFlight(flight_id, flight);
+            if(flightDAO.getFlightById(flight_id).getDeparture_city().equals(flight.getDeparture_city()) &&
+            flightDAO.getFlightById(flight_id).getArrival_city().equals(flight.getArrival_city()))
+                return flightDAO.getFlightById(flight_id);
+        }
         return null;
     }
 
@@ -80,7 +88,7 @@ public class FlightService {
      * @return all flights in the database.
      */
     public List<Flight> getAllFlights() {
-        return null;
+        return flightDAO.getAllFlights();
     }
 
     /**
@@ -92,6 +100,6 @@ public class FlightService {
      * @return all flights departing from departure_city and arriving at arrival_city.
      */
     public List<Flight> getAllFlightsFromCityToCity(String departure_city, String arrival_city) {
-        return null;
+        return flightDAO.getAllFlightsFromCityToCity(departure_city, arrival_city);
     }
 }
